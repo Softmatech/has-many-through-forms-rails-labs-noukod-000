21b5c574090863e41d30bbb19ab8e838a748beb9
Andy Feidje Joseph <ingandy75@gmail.com>
2019-10-22 21:02:15 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,15 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def comment_count
+      comments.length
+  end
+
+  def categories_attributes= category_attrs
+    category_attrs.value.each do |cat_attr|
+      self.categories << Category.find_or_create_by(cat_attr)
+    end
+    
+  end
 
 end
